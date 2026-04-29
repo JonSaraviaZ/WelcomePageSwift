cb18f159e9520f9e31ea04e45bbd06d6d3784b8a
jonathan <jonathan.saravia.223@gmail.com>
2026-04-29 17:55:58 -0400

    first commit

---
+++ OnboardingFlow/WelcomePage.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomePage.swift
+//  OnboardingFlow
+//
+//  Created by Jonathan Saravia Zurita on 29-04-26.
+//
+
+import SwiftUI
+
+struct WelcomePage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WelcomePage()
+}
